b87d31021f281b336a2fa139cc9cde8e03f9b5f8
Felix Schneider <darthrake@yasit.net>
2012-02-15 01:10:11 +0100

    changed direct control animation to reflect real speed

--- js/app_directcontrol.js
+++ js/app_directcontrol.js
@@ -1,3 +1,6 @@
+var old_x=0;
+var old_y=0;
+
 $(document).ready(function(){
 
   $("#cutting_area").click(function(e) {
@@ -16,6 +19,15 @@ $(document).ready(function(){
   	if (lasaur_coord_x > 1220) { lasaur_coord_x = 1220; }
   	if (lasaur_coord_y > 620) { lasaur_coord_y = 620; }
 	
+    // good old pythagoras
+    var length = Math.sqrt(Math.pow(Math.abs(lasaur_coord_x-old_x), 2) + Math.pow(Math.abs(lasaur_coord_y-old_y), 2));
+    var time = length/$( "#feedrate_field" ).val()*60*1000;
+
+    console.log('old_x: ' + old_x + ' old_y: ' + old_y + ' x: ' + lasaur_coord_x + ' y: ' + lasaur_coord_y + ' length: ' + length + ' time: ' + time);
+
+    old_x = lasaur_coord_x;
+    old_y = lasaur_coord_y;
+
   	var g0_or_g1
   	if($('#radio_g1').is(":checked")){
   		g0_or_g1 = 'G1';
@@ -31,7 +43,7 @@ $(document).ready(function(){
   	$.get('/gcode/'+ gcode, function(data) {
   		if (data != "") {
   			$().uxmessage('success', "G-Code sent to serial.");
-  			moveto(x, y);
+  			moveto(x, y, time);
   		} else {
   			$().uxmessage('error', "Serial not connected.");
   		}
@@ -59,15 +71,15 @@ $(document).ready(function(){
     }
   );	
 
-  function moveto (x, y) {
+  function moveto (x, y, time) {
   	$('#y_cart').animate({  
   		top: y - 8.5 - 6
-  	}); 	
+  	}, time); 	
 	
   	$('#x_cart').animate({  
   		left: x - 6,  
   		top: y - 6 
-  	});
+  	}, time);
   };
 
 });  // ready
